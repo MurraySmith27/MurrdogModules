83c073867b891f480b2fa4668e920ede238ade2a
Murray Smith <murraysmith2727@gmail.com>
2025-03-26 18:36:25 -0400

    rect trasnform utils better update method singleton

--- General/RectTransformUtils.cs
+++ General/RectTransformUtils.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public static class RectTransformUtils
 {
 
+    public static List<RectTransform> GetMouseOverRectTransforms()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raycastResults);
+        
+        return raycastResults.Select((RaycastResult result) => { return result.gameObject.transform as RectTransform; }).ToList();
+    }
+    
     public static bool IsMouseOverRectTransform(RectTransform rectTransform)
     {
         PointerEventData eventData = new PointerEventData(EventSystem.current);
