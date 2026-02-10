b3359d413e39b4d9a55c111268745ba2cb094d04
Murray Smith <murraysmith2727@gmail.com>
2026-02-10 12:32:56 -0500

    yes no popup

---
+++ UI/UIPopups/UIYesNoPromptType.cs
@@ -0,0 +1,17 @@
+public enum UIYesNoPromptType
+{
+    GENERIC,
+    CONFIRM,
+    WARNING,
+    DESTRUCTIVE,
+    SAVE,
+    QUIT,
+    RETRY,
+    OVERWRITE,
+    RESET,
+    CUSTOM_1,
+    CUSTOM_2,
+    CUSTOM_3,
+    CUSTOM_4,
+    CUSTOM_5,
+}
